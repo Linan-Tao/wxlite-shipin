cde928c33a3ce108c540f75041398a2fbe3d9b2c
beitaz <beitaz@icloud.com>
2018-04-06 13:56:27 +0800

    添加获取默认地址接口

--- src/api/address_book.js
+++ src/api/address_book.js
@@ -4,6 +4,13 @@ const all = () => {
   return http.get('/address_books')
 }
 
+/**
+ * 获取默认地址
+ */
+const common = () => {
+  return http.get('/address_books/common')
+}
+
 const createAddressBook = (options) => {
   return http.post(`/address_books`, options)
 }
@@ -12,4 +19,4 @@ const find = (id) => {
   return http.get(`/address_books/${id}`)
 }
 
-export default { all, find, createAddressBook }
+export default { all, find, common, createAddressBook }
